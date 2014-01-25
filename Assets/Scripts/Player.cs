b958180b96eb0a254f4db6afbac1d3ff8f6fdfd3
fpg1503 <fpg1503@gmail.com>
2014-01-25 16:40:46 -0200

    Updated Player and Unit Classes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Player : MonoBehaviour
+{
 	
+	public enum ColorType
+	{
+		green,
+		red,
+		yellow,
+		blue
+	}
+	
+	private CardList cards;
+	private List<Guy> guys;
+	private ColorType color;
+	public OTSprite _sprite;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		this.cards = new CardList();
+		this.guys = new Array<Guy>();
+	}
+	
+	public CardList getCards ()
+	{
+		return this.cards;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Unit : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Unit : MonoBehaviour
+{
+	private Player owner;
 	
+	// Use this for initialization
+	void Start ()
+	{
+		this.owner = null;
+	}
+	
+	public void setOwner (Player owner)
+	{
+		this.owner = owner;
+		
+		//play ownership change animation
+	}
+	
+	public Player getOwner ()
+	{
+		return this.owner;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		
 	}
 }
